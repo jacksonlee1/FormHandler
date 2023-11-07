0f2752fc853c0d01438d1e9ae678c3b9815c04a1
Jackson Lee <jacksonlee050802@gmail.com>
2023-11-07 15:50:16 -0500

    fix password

--- FileHandler/FileHandler/obj/Debug/net7.0/FileHandler.AssemblyInfo.cs
+++ FileHandler/FileHandler/obj/Debug/net7.0/FileHandler.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("63c0d260-d42f-4ea2-9ba7-bf760cc90698")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileHandler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
